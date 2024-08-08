076c2003f27df836e9ed5e3fda80cbd1b4144b33
Filipe Carvalho <filipe.carvalho@nos.pt>
2024-08-08 20:19:37 +0100

    fix dependencies

--- otel-log-wrapper/setup.py
+++ otel-log-wrapper/setup.py
@@ -8,10 +8,8 @@ setup(
     author_email='filipe.carvalho@nos.pt',
     description='Description of my package',
     packages=find_packages(),
-    install_requires=['opentelemetry._logs',
-                      'opentelemetry.exporter.otlp.proto.http._log_exporter',
-                      'opentelemetry.instrumentation.logging',
-                      'opentelemetry.sdk._logs',
-                      'opentelemetry.sdk._logs.export',
-                      'opentelemetry.sdk.resources'],
+    install_requires=[
+        'opentelemetry-exporter-otlp',
+        'opentelemetry-instrumentation-logging'
+    ],
 )
